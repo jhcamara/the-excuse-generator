91580b605b1766719db63d1d25a868653b835bf9
jhcamara <jhcamara@gmail.com>
2022-11-11 18:34:47 +0000

    submitting exercise

--- src/app.js
+++ src/app.js
@@ -8,4 +8,49 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  function random_item(items) {
+    return items[Math.floor(Math.random() * items.length)];
+  }
+
+  let who = [
+    "Putin",
+    "My iguana",
+    "The government",
+    "My teacher",
+    "Jennifer Lawrence"
+  ];
+  let action = ["ate", "ran over", "sold", "gave away", "burned", "wet"];
+  let what = [
+    "my gin tonic",
+    "my car",
+    "my money",
+    "my alarm clock",
+    "my tamagotchi"
+  ];
+  let when = [
+    "while I was sleeping",
+    "last Christmas",
+    "when I was not watching",
+    "on my birthday",
+    "during breakfast"
+  ];
+
+  let finalExcuse =
+    random_item(who) +
+    " " +
+    random_item(action) +
+    " " +
+    random_item(what) +
+    " " +
+    random_item(when);
+
+  console.log(finalExcuse);
+
+  //Parsing into HTML
+  document.getElementById("excuse").innerHTML = finalExcuse;
+
+  function refresh() {
+    window.location.reload("Refresh");
+  }
 };
